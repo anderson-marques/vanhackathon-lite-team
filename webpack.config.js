b9fb627e021ce4b5e7f7706931eabff517654721
anderson-marques <anderson@kinatec.com.br>
2017-04-07 23:19:56 -0300

    adicionado git ignore

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/*
+ * Webpack configuration file. Specifies source files and distribution folder
+ * */
+var config = {
+    context: __dirname + "/app",
+    entry: "./main.js",
+
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
+module.exports = config;
